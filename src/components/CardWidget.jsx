20d3a018f0f37bb89036acce84c524e6ffca46ee
PcFijaMartex <germanguti1998@hotmail.com>
2024-06-04 16:43:25 -0300

    Second commit

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -3,7 +3,7 @@ const CardWidget = () => {
   return (
     <div>
       <img src="" alt="" /> // Imagen de carrito 
-      <p>10</p> //De momento, numero de carrito fijo
+      <p>10</p> //De momento, numero de carrito fijo :)
     </div>
   )
 }
