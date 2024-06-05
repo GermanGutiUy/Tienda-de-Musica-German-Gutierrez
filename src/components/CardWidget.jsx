3cea5ad4dad9fab205448cd981ba133a6531995f
PcFijaMartex <germanguti1998@hotmail.com>
2024-06-05 10:27:55 -0300

    Tercer Commit

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -1,9 +1,11 @@
+import { TiShoppingCart } from "react-icons/ti";
 
 const CardWidget = () => {
   return (
     <div>
-      <img src="" alt="" /> // Imagen de carrito 
-      <p>10</p> //De momento, numero de carrito fijo :)
+      <TiShoppingCart />
+      <img src="" alt="" /> {/*// Imagen de carrito */}
+      <p>10</p> {/* //De momento, numero de carrito fijo */}
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import CardWidget from './CardWidget'
+import CardWidget from './CardWidget' //Foto del carrito de compras de ReactIcon
+import Logo from "../img/Logo.jpg"
 
 const navbar = () => {
   return (
           <nav className='navbar'>
         <div className='brand'>
-            <img src="" alt="" />
+            <img width={200} src={ Logo } alt="" />
         </div>
-        <ul> // Lista a agregar
+        <ul>{/* // Lista a agregar */}
             <li>Cuerda</li>
             <li>Viento</li>
             <li>Percusion</li>
             <li>Audio Pro</li>
             <li>Blog</li>
             <li>Contacto</li>
-            <li>Nosotros</li>
         </ul>
-        <CardWidget /> //CardWidget Dentro del Nav
+        <CardWidget /> {/*CardWidget Dentro del Nav */}
 
         <NavBar />
     </nav>
