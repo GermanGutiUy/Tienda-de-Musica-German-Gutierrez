050a407d58e0c165dc24cf4d34a996bd8e28e9cc
PcFijaMartex <germanguti1998@hotmail.com>
2024-06-27 17:16:31 -0300

    Error de link

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ItemDetailContaine from './components/itemDetailContainer/itemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
+import Error from '/components/Error.jsx'
+ 
 function App() {
   return (
     <BrowserRouter>
@@ -13,7 +14,7 @@ function App() {
         <Route path = "/" element = {<ItemListContainer />} />
         <Route path = "/categoria/:idCategoria" element = {<ItemListContainer />}/>
         <Route path = "/detalle/:idProdcuto" element = {<ItemDetailContaine />}/>
-
+        <Route path = "/detalle/:idProdcuto" element = {<Error />}/>
       </Routes>
 
     </BrowserRouter>
---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const error = () => {
+  return (
+    <div>
+        <h2>Eror 404 Pagina no encontrada</h2>
+    </div>
+  )
+}
+
+export default error
