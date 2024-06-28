8736c69dc2b1dc5e6efaac068249d18d821befec
PcFijaMartex <germanguti1998@hotmail.com>
2024-06-28 08:58:53 -0300

    Modificacion de Item

--- src/components/ItemListContainer/item.jsx
+++ src/components/ItemListContainer/item.jsx
@@ -3,17 +3,16 @@ import { Link } from 'react-router-dom'
 
 const item = ({ prodcuto }) => {
   return (
-    <div>
+    <Link to = {"/detalle/" + prodcuto.id} className = "item">
         <img src={prodcuto.imagen} width = {200} alt =""/>
         <div className='descriptiocn-item'>
           <p>{prodcuto.nombre}</p>
           <p>${prodcuto.precio}</p>
-
-          <Link to={"/detalle/" + prodcuto.id}>Ver detalles</Link>
-
         </div>
-    </div>
+    </Link>
   )
 }
 
 export default item
+
+// <Link to={"/detalle/" + prodcuto.id}>Ver detalles</Link>
