00a3e23602db9d53aa51bdb8bd016b7ca80dea26
PcFijaMartex <germanguti1998@hotmail.com>
2024-07-08 12:25:12 -0300

    Diseños 2

--- src/components/NavBar/CardWidget.jsx
+++ src/components/NavBar/CardWidget.jsx
@@ -5,11 +5,9 @@ const CardWidget = () => {
   return (
     <div className="CardWidgetContainer">
       <div className="CardWidgetImg">
-      <div>
         <GiShoppingCart size={70}/>
       </div>
       <p className="CardWidgetCounter">10</p> {/* //De momento, numero de carrito fijo */}
-      </div>
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,69 +1,78 @@
 import React from 'react';
 import CardWidget from './CardWidget'; // Import Foto del carrito de compras
 import Logo from "../../img/Logo.jpg"; // Import Foto del logo
-import './NavBar.css'
-import {  Link  } from "react-router-dom"
+import './NavBar.css';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-
     <nav className='NavBar'>
-      <Link  to = "/"className='band'>
+      <Link to="/" className='brand'>
         <img width={100} src={Logo} alt="Logo" />
       </Link>
 
+      <ul className='navLinks'>
+        <li>
+          <Link to="/categoria/cuerda" className="category">
+            <p>Cuerda</p>
+            <div className="img-category">
+              <img
+                src=""
+                alt=""
+              />
+            </div>
+          </Link>
+        </li>
 
-      <ul>
-        <Link to = "/categoria/cuerda" className = "category">
-          <p>Cuerda</p>
-        <div className = "img-category">
-          <img 
-            src = ""
-            alt = ""
-            />
-        </div>
-        </Link>
+        <li>
+          <Link to="/categoria/viento" className="category">
+            <p>Viento</p>
+            <div className="img-category">
+              <img
+                src=""
+                alt=""
+              />
+            </div>
+          </Link>
+        </li>
 
-        <Link to = "/categoria/viento" className = "category">
-          <p>Viento</p>
-        <div className = "img-category">
-          <img 
-            src = ""
-            alt = ""
-            />
-        </div>
-        </Link>
+        <li>
+          <Link to="/categoria/percusion" className="category">
+            <p>Percusion</p>
+            <div className="img-category">
+              <img
+                src=""
+                alt=""
+              />
+            </div>
+          </Link>
+        </li>
 
-        <Link to = "/categoria/percusion" className = "category">
-          <p>Percusion</p>
-        <div className = "img-category">
-          <img 
-            src = ""
-            alt = ""
-            />
-        </div>
-        </Link>
-        
-        <Link to = "/categoria/audiopro" className = "category">
-          <p>Audio Pro</p>
-        <div className = "img-category">
-          <img 
-            src = ""
-            alt = ""
-            />
-        </div>
-        </Link>
+        <li>
+          <Link to="/categoria/audiopro" className="category">
+            <p>Audio Pro</p>
+            <div className="img-category">
+              <img
+                src=""
+                alt=""
+              />
+            </div>
+          </Link>
+        </li>
 
-        <Link to = "/categoria/blog" className = "category">
-          <p>Blog</p>
-        <div className = "img-category">
-          <img 
-            src = ""
-            alt = ""
-            />
-        </div>
-        </Link>
+        <li>
+          <Link to="/categoria/blog" className="category">
+            <p>Blog</p>
+            <div className="img-category">
+              <img
+                src=""
+                alt=""
+              />
+            </div>
+          </Link>
+        </li>
       </ul>
+
       <CardWidget />
     </nav>
   );
