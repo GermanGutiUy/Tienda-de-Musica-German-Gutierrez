d0cdca2022593a51c70b9e856b27067b5cc25aa3
PcFijaMartex <germanguti1998@hotmail.com>
2024-07-08 13:43:20 -0300

    Loading Detail

--- src/components/itemDetailContainer/itemDetailContainer.jsx
+++ src/components/itemDetailContainer/itemDetailContainer.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import {obtenerProductos} from "../../data/data.js"
 import ItemDetail from './ItemDetail.jsx'
 import { useParams } from 'react-router-dom'
+import useLoading from '../hooks/useLoading.jsx';
+import Loading from '../ItemListContainer/loading.jsx'; 
 import './ItemDetailContainer.css'
 
 const ItemDetailContainer = () => {
@@ -10,18 +12,31 @@ const ItemDetailContainer = () => {
 
     const { idProducto } = useParams()
 
+    const { isLoading, showLoading, hideLoading } = useLoading(); //aca
+
     useEffect (() =>{
+       // Mostrar pantalla de carga
+       showLoading();
+
         obtenerProductos()
         .then((data) =>  {
             const productoFiltrado = data.find((productoData) => productoData.id === idProducto)
             serProducto (productoFiltrado)
         })
+        .finally(() => {
+          // Ocultar pantalla de carga
+          hideLoading();
+      });
     }, [])
 
 
 
   return (
-    <ItemDetail producto = {producto} />
+    <div className="itemlistcontainer">
+    {isLoading ? <Loading /> : <ItemDetail producto = {producto} />}
+    </div>
+
+
   )
 }
 
