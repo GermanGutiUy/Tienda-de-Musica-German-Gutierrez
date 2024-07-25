875192486a79dcf0b801d36720a351d7becd4c09
PcFijaMartex <germanguti1998@hotmail.com>
2024-07-25 15:06:55 -0300

    Arreglo de filtro

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,54 +1,54 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 
-import db from "../../db/db.js"
+import db from "../../db/db.js";
 import ItemList from "../ItemListContainer/itemList.jsx";
 import useLoading from '../hooks/useLoading.jsx';
 import Loading from './loading.jsx';
-//import { getProducts,getProductsById, addProduct, modProduct } from "../utils/fetchApi.js" | Funcion para base de datos en apis
 
 import "./itemlistcontainer.css";
 
 const ItemListContainer = () => {
     const [productos, setProductos] = useState([]);
-
     const { idCategoria } = useParams();
-
     const { isLoading, showLoading, hideLoading } = useLoading();
 
-    const obtenerProductos = () => {
-        const productosRef = collection( db, "productos")
-        getDocs(productosRef)
-            .then((respuesta) => {
-                const data = respuesta.docs.map((productDb) => {
-                    return { id: productDb.id, ...productDb.data() }
-                })
-                setProductos(data)
-            })
-    }
-    
-    const obtenerProductosFiltrados = () => {
-        const productosRef = collection(db, "productos")
-        const q = query(productosRef, where("categoria", "==", idCategoria))
-        getDocs(q)
-            .then((respuesta) => {
+    const obtenerProductos = async () => {
+        showLoading();
+        try {
+            const productosRef = collection(db, "productos");
+            const respuesta = await getDocs(productosRef);
+            const data = respuesta.docs.map((productDb) => {
                 return { id: productDb.id, ...productDb.data() };
             });
-            setProductos(data)
+            setProductos(data);
+        } finally {
+            hideLoading();
         }
+    };
+
+    const obtenerProductosFiltrados = async () => {
+        showLoading();
+        try {
+            const productosRef = collection(db, "productos");
+            const q = query(productosRef, where("categoria", "==", idCategoria));
+            const respuesta = await getDocs(q);
+            const data = respuesta.docs.map((productDb) => {
+                return { id: productDb.id, ...productDb.data() };
+            });
+            setProductos(data);
+        } finally {
+            hideLoading();
+        }
+    };
 
     useEffect(() => {
-
         if (idCategoria) {
-            obtenerProductosFiltrados()
+            obtenerProductosFiltrados();
         } else {
             obtenerProductos();
         }
-
-        obtenerProductos()
-
     }, [idCategoria]);
 
     return (
